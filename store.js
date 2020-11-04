b21b5862b58f1b43f142677d0be6cdcfcb47e4e7
psarge2 <54369285+psarge2@users.noreply.github.com>
2020-11-04 16:47:08 -0500

    store.js file content

---
+++ store.js
@@ -0,0 +1,36 @@
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready()
+}
+
+
+
+var removeCartItemButtons = document.getElementsByClassName('btn-danger')
+
+for (var i = 0; i < removeCartItemButtons.length; i++) {
+    var button = removeCartItemButtons[i]
+    button.addEventListener('click', function(event){
+        var buttonClicked = event.target;
+        buttonClicked.parentElement.parentElement.remove();
+        updateCartTotal();
+    })
+}
+
+function updateCartTotal() {
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    var total = 0 
+    for (var i = 0; i < cartRows.length; i++) {
+        var cartRow = cartRows[i]
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+        console.log(priceElement, quantityElement)
+        var price = parseFloat(priceElement.innerText.replace('$', ''))
+        console.log(price)
+        var quantity = quantityElement.value 
+        total = total + (price * quantity)
+    }
+    document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
+}
+
